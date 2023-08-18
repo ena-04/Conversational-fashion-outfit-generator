2ebfcd7bccb4c21228df506aa55862b94a9c7f95
Enakkhi Saha <sahaenakkhi@gmail.com>
2023-08-18 21:46:26 +0530

    added changes

---
+++ server/secondary_testers/scraping_flipkart.py
@@ -0,0 +1,44 @@
+item_list=['Anarkali suit in pink, black, and purple with a matching salwar.', 'Pumps in black or purple.', 'Traditional jewelry such as a necklace, earrings, and bangles.', 'A bindi and mehndi to complete the look.']
+
+
+# * A long, pink maxi dress with a black belt and a slit up the side.
+
+# * Black high heels or sandals.
+
+# * A small, black clutch purse.
+
+# * A silver necklace and bracelet.
+
+# * Pink earrings.
+
+import spacy
+import string
+
+# Load the English language model
+nlp = spacy.load("en_core_web_sm")
+
+# Define your custom stopwords
+custom_stopwords = ['matching','complete','look']
+
+# Define a translation table to remove punctuations
+translator = str.maketrans("", "", string.punctuation)
+
+# Extend the stopwords list
+nlp.Defaults.stop_words |= set(custom_stopwords)
+
+filtered_list=[]
+
+for item in item_list:
+
+    # Process the text with spaCy
+    doc = nlp(item)
+
+    # Create a list of tokens without stopwords and punctuations
+    filtered_tokens = [token.text.translate(translator) for token in doc if not token.is_stop]
+
+    # Join the filtered tokens back into a sentence
+    filtered_sentence = ' '.join(filtered_tokens)
+
+    print(filtered_sentence)
+
+
