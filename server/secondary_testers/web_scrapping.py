4292f0a9de9e354857c61898527690f3df486a9c
Rahul <rb341047@gmail.com>
2023-08-20 03:44:43 +0530

    flipkart

--- server/secondary_testers/web_scrapping.py
+++ server/secondary_testers/web_scrapping.py
@@ -1,32 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import unquote
+from flask import Flask, render_template
+from filters import flipkart_url
 
-# Assuming you have the 'flipkart_url' generated from your previous code
-flipkart_url = "..."  # Your Flipkart URL here
+app = Flask(__name__)
 
-# Perform web scraping
-response = requests.get(flipkart_url)
-soup = BeautifulSoup(response.content, 'html.parser')
+def scrape_flipkart():
+    base_url = flipkart_url  # Use the Flipkart URL from filters.py
+    response = requests.get(base_url)
 
-# Extract image URLs and product links
-image_urls = []
-product_links = []
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        product_containers = soup.find_all("div", class_="_1AtVbE")
 
-# Assuming you have identified the HTML elements that contain images and links
-# You will need to inspect the HTML structure of the Flipkart search results page
-# and adjust the following code accordingly
+        results = []
 
-for image_tag in soup.find_all('img', class_='...'):  # Replace '...' with the actual class or attribute
-    image_urls.append(image_tag['src'])
+        for container in product_containers[:3]:  # Get top 3 products
+            title = container.find("a", class_="IRpwTa")
+            if title:
+                title = title.text
 
-for link_tag in soup.find_all('a', class_='...'):  # Replace '...' with the actual class or attribute
-    product_links.append(link_tag['href'])
+            image_tag = container.find("img", class_="_396cs4")
+            if image_tag:
+                img_url = image_tag.get("src")
+                img_url = unquote(img_url)
 
-# Display images and links
-html_content = ""
-for image_url, product_link in zip(image_urls, product_links):
-    html_content += f'<a href="{product_link}" target="_blank"><img src="{image_url}" alt="Product Image"></a><br>'
+            product_link = container.find("a", class_="_2UzuFa")
+            if product_link:
+                product_link = product_link["href"]
 
-# Create an HTML file
-with open('flipkart_results.html', 'w') as html_file:
-    html_file.write(html_content)
+            if title and img_url and product_link:
+                results.append({
+                    'product_title': title,
+                    'image_url': img_url,
+                    'product_link': product_link
+                })
+
+        return results
+    else:
+        return []
+
+@app.route('/')
+def index():
+    results = scrape_flipkart()
+    return render_template('index.html', results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
